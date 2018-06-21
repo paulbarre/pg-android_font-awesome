8a01ddfb3933853a50bbb429aeae288a5bbe37fb
Paul Barré <paul.yvon.barre@gmail.com>
2018-06-21 15:01:20 +0900

    Using FA, 2 ways of setting it on Text Views

--- app/src/main/java/co/paulbarre/pg_android_font_awesome/MainActivity.kt
+++ app/src/main/java/co/paulbarre/pg_android_font_awesome/MainActivity.kt
@@ -1,12 +1,17 @@
 package co.paulbarre.pg_android_font_awesome
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.res.ResourcesCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val typeface = Typeface.createFromAsset(assets, "font/fa-solid-900.ttf")
+        middle.typeface = ResourcesCompat.getFont(this, R.font.fa_solid_900)
     }
 }
